bce05c846c097661513dfd2d9996b2fc30c971f3
nik39n <40721920+nik39n@users.noreply.github.com>
2021-01-31 08:28:14 +0200

    about last

---
+++ Nikita_Ababii/5_task_section/520.solution.js
@@ -0,0 +1,4 @@
+// 5.20 Дан объект {js:['jQuery', 'Angular'], php: 'hello', css: 'world'}. Выведите с его помощью
+// слово 'jQuery'.
+let obj = {js:['jQuery', 'Angular'], php: 'hello', css: 'world'};
+console.log(obj.js[0]);
